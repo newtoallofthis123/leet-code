7e58ce10746a3dc51fd4a512e772c3d15cf73919
Noob Science <noobscience123@gmail.com>
2023-10-02 15:36:14 +0530

    Gopher baby!!

---
+++ python/medium/champagne-tower.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def champagneTower(self, poured, query_row, query_glass):
+        """
+        :type poured: int
+        :type query_row: int
+        :type query_glass: int
+        :rtype: float
+        """
+
+
+solution = Solution()
+print(solution.champagneTower(2, 1, 1))
